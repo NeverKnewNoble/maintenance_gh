ed7c82481045b1c6c6bcf9db278f20bfdbfd2175
Larry-Noble <nortexnoble@.com>
2023-10-06 18:24:33 +0000

    after completed the shell of the request and schedule ready to go into detail

--- maintenance_gh/maintenance_gh/doctype/maintenance_request/maintenance_request.js
+++ maintenance_gh/maintenance_gh/doctype/maintenance_request/maintenance_request.js
@@ -1,8 +1,34 @@
 // Copyright (c) 2023, Larry-Noble and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on('Maintenance Request', {
-	// refresh: function(frm) {
+// frappe.ui.form.on('Maintenance Request', {
+// 	// refresh: function(frm) {
 
-	// }
+// 	// }
+// });
+
+frappe.ui.form.on('Maintenance Request', {
+    refresh: function(frm) {
+        if (!frm.doc.__islocal) {
+            // If the document is not new, don't make any changes
+            return;
+        }
+        if (frm.doc.__islocal) {
+            // Set the current date in the date_requested field
+            frm.set_value('date_requested', frappe.datetime.nowdate());
+        }
+
+        // Set the issue_raised_by field with the username of the logged-in user
+        frm.set_value('issue_raised_by', frappe.session.user);
+    }
 });
+
+// frappe.ui.form.on('Maintenance Request', {
+//     refresh: function(frm) {
+//         // Check if the document is new
+//         if (frm.doc.__islocal) {
+//             // Set the current date in the date_requested field
+//             frm.set_value('date_requested', frappe.datetime.nowdate());
+//         }
+//     }
+// });
---
+++ maintenance_gh/maintenance_gh/notification/maintain_scheduling_mail/maintain_scheduling_mail.md
@@ -0,0 +1,6 @@
+<h3>Maintenance Scheduling From Maintenance Team<h3>
+
+<p>Report {{ doc.name }} has been submitted to the maintenance team<p>
+
+<h4>Scheduling Details:<h4>
+
