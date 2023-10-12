a7b774751d9ef9951514c6c795363b3f5d36f75b
Carbonit Solutions <admin@carbonitesolutions.com>
2023-10-12 17:26:11 +0000

    Maintance push with Automation

--- maintenance_gh/maintenance_gh/doctype/maintenance_request/maintenance_request.js
+++ maintenance_gh/maintenance_gh/doctype/maintenance_request/maintenance_request.js
@@ -22,3 +22,32 @@ frappe.ui.form.on('Maintenance Request', {
         frm.set_value('issue_raised_by', frappe.session.user);
     }
 });
+
+
+frappe.ui.form.on('Maintenance Request', {
+    on_submit: function(frm) {
+        frappe.call({
+            method: 'maintenance_gh.maintenance_gh.doctype.maintenance_request.maintenance_request.create_scheduling_documents',
+            args: { doc: frm.doc },
+            callback: function(r) {
+                if (!r.exc) {
+                    frappe.msgprint('Maintenance Scheduling documents created successfully.');
+                }
+            }
+        });
+    },
+    refresh: function(frm) {
+        if (!frm.doc.__islocal) {
+            // If the document is not new, don't make any changes
+            return;
+        }
+        if (frm.doc.__islocal) {
+            // Set the current date in the date_requested field
+            frm.set_value('date_requested', frappe.datetime.nowdate());
+        }
+
+        // Set the issue_raised_by field with the username of the logged-in user
+        frm.set_value('issue_raised_by', frappe.session.user);
+    },
+
+});
--- maintenance_gh/maintenance_gh/doctype/maintenance_request/maintenance_request.py
+++ maintenance_gh/maintenance_gh/doctype/maintenance_request/maintenance_request.py
@@ -1,66 +1,34 @@
-# Copyright (c) 2023, Larry-Noble and contributors
-# For license information, please see license.txt
+# # Copyright (c) 2023, Larry-Noble and contributors
+# # For license information, please see license.txt
 
 
 
-# Patrick code>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-# import frappe
-# from frappe.model.document import Document
+# # Patrick code>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+# # import frappe
+# # from frappe.model.document import Document
 
 
 
-# def maintenance(doc, event):
-#     if event == 'on_submit':
+# # def maintenance(doc, event):
+# #     if event == 'on_submit':
 
-#         maintenance_scheduling = frappe.new_doc('Maintenance Scheduling')
+# #         maintenance_scheduling = frappe.new_doc('Maintenance Scheduling')
 
-#         maintenance_scheduling.ms_data = doc.issue_raised_by
+# #         maintenance_scheduling.ms_data = doc.issue_raised_by
 
-#         maintenance_scheduling.insert()
-#         maintenance_scheduling.submit()
+# #         maintenance_scheduling.insert()
+# #         maintenance_scheduling.submit()
 
     
 
 
-# class MaintenanceRequest(Document):
-#     def on_submit(self):
-#         maintenance(self, 'on_submit')
+# # class MaintenanceRequest(Document):
+# #     def on_submit(self):
+# #         maintenance(self, 'on_submit')
         
 
 
-# trial 1>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
-import frappe
-from frappe.model.document import Document
-from frappe import get_doc, core
-
-
-
-def maintenance(doc, event):
-    if event == 'on_submit':
-
-        maintenance_scheduling = frappe.new_doc('Maintenance Scheduling')
-
-        source_doc = get_doc('Maintenance Request', MaintenanceRequest)
-        source_child_table = source_doc.sector
-        
-        target_doc = get_doc('Maintenance Scheduling',maintenance_scheduling)
-
-        target_doc.sector_1 = source_child_table
-
-        
-        maintenance_scheduling.insert()
-        maintenance_scheduling.submit()
-
-
-class MaintenanceRequest(Document):
-    def on_submit(self):
-        maintenance(self, 'on_submit')
-
-
-
-
-
-# Almost there>>>>>>>>>>>>>>>>>>>>
+# # trial 1>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
 # import frappe
 # from frappe.model.document import Document
 # from frappe import get_doc, core
@@ -78,12 +46,10 @@ class MaintenanceRequest(Document):
 #         target_doc = get_doc('Maintenance Scheduling',maintenance_scheduling)
 
 #         target_doc.sector_1 = source_child_table
-#         target_doc.issue_description = source_child_table
 
+        
 #         maintenance_scheduling.insert()
 #         maintenance_scheduling.submit()
-
-    
 
 
 # class MaintenanceRequest(Document):
@@ -93,6 +59,56 @@ class MaintenanceRequest(Document):
 
 
 
+
+# # Almost there>>>>>>>>>>>>>>>>>>>>
+# # import frappe
+# # from frappe.model.document import Document
+# # from frappe import get_doc, core
+
+
+
+# # def maintenance(doc, event):
+# #     if event == 'on_submit':
+
+# #         maintenance_scheduling = frappe.new_doc('Maintenance Scheduling')
+
+# #         source_doc = get_doc('Maintenance Request', MaintenanceRequest)
+# #         source_child_table = source_doc.sector
+        
+# #         target_doc = get_doc('Maintenance Scheduling',maintenance_scheduling)
+
+# #         target_doc.sector_1 = source_child_table
+# #         target_doc.issue_description = source_child_table
+
+# #         maintenance_scheduling.insert()
+# #         maintenance_scheduling.submit()
+
+    
+
+
+# # class MaintenanceRequest(Document):
+# #     def on_submit(self):
+# #         maintenance(self, 'on_submit')
+
+
+
+import frappe
+from frappe.model.document import Document
+
+class MaintenanceRequest(Document):
+    pass
+
+@frappe.whitelist()
+def create_scheduling_documents(doc):
+    # Deserialize the JSON string into a Python dictionary
+    doc = frappe.parse_json(doc)
+
+    if doc.get("sector"):
+        for row in doc.get("sector"):
+            scheduling_doc = frappe.new_doc("Maintenance Scheduling")
+            scheduling_doc.sector_name = row.get("sector_1")
+            scheduling_doc.description_scheduling = row.get("issue_description")
+            scheduling_doc.save()
 
 
 
