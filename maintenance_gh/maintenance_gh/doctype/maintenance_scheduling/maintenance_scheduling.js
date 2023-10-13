66f928186b3d42457822ad667cd71418e26a1a64
Larry-Noble <nortexnoble@gmail.com>
2023-10-13 00:17:55 +0000

    after making the button going to rescheduling

--- maintenance_gh/maintenance_gh/doctype/maintenance_scheduling/maintenance_scheduling.js
+++ maintenance_gh/maintenance_gh/doctype/maintenance_scheduling/maintenance_scheduling.js
@@ -2,9 +2,12 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Maintenance Scheduling', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		frm.add_custom_button(__('Reschedule This Issue'), function(){
+		  frappe.msgprint(frm.doc.email);
+		  frappe.set_route("Form", "Maintenance Rescheduling");
+	  }, __("Reschedule"));
+	}
 });
 
 
